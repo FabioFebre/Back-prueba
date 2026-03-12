179a7ad6c082e242ee4d5808052592df5a4afc9a
Fabio Febre <febrevargasf@gamil.com>
2026-03-12 16:48:20 -0500

    12-4

--- routes/producto.js
+++ routes/producto.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Producto, Categoria,Variante  } = require('../models');
+const { Producto, Categoria,Variante ,sequelize  } = require('../models');
 const multer = require('multer');
 const path = require('path');
 const upload = require('../middlewares/upload');
