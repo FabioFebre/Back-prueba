8535973a7ca68b237e60fe034f9573ba0f3fb9a8
FabioFebre <febrevargasf@gmail.com>
2026-05-20 15:09:49 -0500

    Corrigiendo espacio de imagen

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -20,7 +20,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 10 * 1024 * 1024, 
+    fileSize: 15 * 1024 * 1024,  
     files: 10,                
   },
 });
